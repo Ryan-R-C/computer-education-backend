9deb7f09672673288bd4fbb33e7210b94beb1aa7
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:17:28 -0300

    create User's model

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    email: {type: String},
+    nome: {type: String},
+    nickName: {type: String},
+  }
+);
+
+const user = mongoose.model('user', UserSchema);
+
+export default user;
