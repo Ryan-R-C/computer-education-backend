116d127ba36e2351fcdabb617d70fbb42e722fa8
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:20:24 -0300

    created the routes' index file

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import options from "./optionsRoutes.js"
+import subtasks from "./subtasksRoutes.js"
+import questions from "./questionsRoutes.js"
+import tasks from "./tasksRoutes.js"
+import users from "./usersRoutes.js"
+import userscores from "./userScoresRoutes.js"
+
+const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({titulo: "Curso de node"})
+  })
+
+  app.use(
+    express.json(),
+    userscores,
+    users,
+    options,
+    questions,
+    subtasks,
+    tasks
+  )
+}
+
+export default routes
