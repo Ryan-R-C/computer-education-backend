55d7a753d66b5c099859e53df56c3be1877ee510
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:16:14 -0300

    created the db connection file

---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+const urlConnection = "mongodb+srv://ryan:Ryansp123@cluster0.dgsm3wt.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(urlConnection);
+
+let db = mongoose.connection;
+
+export default db;
