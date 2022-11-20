7c4ebc2c3bf3e260d6526f809084f805e1a543b7
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:20:39 -0300

    created the app's  file

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from 'cors';
+import db from "./config/dbConnect.js"
+import routes from "./routes/index.js"
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso')
+})
+
+
+const app = express();
+
+// enable all acess origins
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
+
+// Enables CORS
+app.use(cors({ origin: true }));
+
+app.use(express.json())
+
+routes(app);
+
+export default app
